a0db31342fe76fc23ddb8169c1183cfef62aae48
Toluwanimi Adeyemo <tolu.adeyemo7@gmail.com>
2022-05-22 23:51:46 +0000

    more redux basics

--- redux-demo.js
+++ redux-demo.js
@@ -1,13 +1,23 @@
 const redux = require("redux");
 
 const counterReducer = (state = { counter: 0 }, action) => {
-  return {
-    counter: state.counter + 1,
-  };
+  if (action.type === "ADD") {
+    return {
+      counter: state.counter + 1,
+    };
+  }
+  
+  if (action.type === "SUBTRACT") {
+    return {
+      counter: state.counter - 1,
+    };
+  }
+
+  return state;
 };
 
 const store = redux.createStore(counterReducer);
-console.log(store.getState());
+// console.log(store.getState());
 
 const counterSubscriber = () => {
   const latestState = store.getState();
@@ -16,4 +26,5 @@ const counterSubscriber = () => {
 
 store.subscribe(counterSubscriber);
 
-store.dispatch({type: 'ADD'})
+store.dispatch({ type: "ADD" });
+store.dispatch({ type: "SUBTRACT" });
