445e2fb1b54f1907e11e24d9adbfc84965a1bf6a
Toluwanimi Adeyemo <tolu.adeyemo7@gmail.com>
2022-05-23 00:15:10 +0000

    creating redux store for react

--- learn-redux/src/components/Counter.js
+++ learn-redux/src/components/Counter.js
@@ -6,7 +6,7 @@ const Counter = () => {
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>-- COUNTER VALUE --</div>
+      <div className={classes.value}> - COUNTER VALUE - </div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
   );
---
+++ learn-redux/src/store/index.js
@@ -0,0 +1,32 @@
+// const redux = require("redux");
+import { createStore } from 'redux';
+
+const counterReducer = (state = { counter: 0 }, action) => {
+  if (action.type === "ADD") {
+    return {
+      counter: (state.counter += 1),
+    };
+  }
+
+  if (action.type === "MINUS") {
+    return {
+      counter: (state.counter -= 1),
+    };
+  }
+
+  return state;
+};
+
+const store = createStore(counterReducer);
+
+// const counterSubscriber = () => {
+//   const latestStoreState = store.getState();
+//   console.log(latestStoreState);
+// };
+
+// store.subscribe(counterSubscriber);
+
+// store.dispatch({ type: "ADD" });
+// store.dispatch({ type: "MINUS" });
+
+export default store;
