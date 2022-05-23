06085f083975170b910e67b9fa7bd66365e55c4d
Toluwanimi Adeyemo <tolu.adeyemo7@gmail.com>
2022-05-23 00:32:30 +0000

    dispatching actions from react components

--- learn-redux/src/components/Counter.js
+++ learn-redux/src/components/Counter.js
@@ -1,8 +1,16 @@
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import classes from "./Counter.module.css";
 
 const Counter = () => {
+  const dispatch = useDispatch();
   const counter = useSelector((state) => state.counter);
+
+  const add = () => {
+    dispatch({ type: "ADD" });
+  };
+  const minus = () => {
+    dispatch({ type: "MINUS" });
+  };
 
   const toggleCounterHandler = () => {};
 
@@ -10,6 +18,10 @@ const Counter = () => {
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
       <div className={classes.value}>{counter} </div>
+      <div>
+        <button onClick={add}>Add</button>
+        <button onClick={minus}>MINUS</button>
+      </div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
   );
