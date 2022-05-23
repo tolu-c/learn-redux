fcd003e02b3b394a6fc9f697d4dc4362699fe66e
Toluwanimi Adeyemo <tolu.adeyemo7@gmail.com>
2022-05-23 00:57:53 +0000

    working with multiple state properties

--- learn-redux/src/components/Counter.js
+++ learn-redux/src/components/Counter.js
@@ -4,6 +4,7 @@ import classes from "./Counter.module.css";
 const Counter = () => {
   const dispatch = useDispatch();
   const counter = useSelector((state) => state.counter);
+  const show = useSelector((state) => state.showCounter);
 
   const add = () => {
     dispatch({ type: "ADD" });
@@ -15,17 +16,21 @@ const Counter = () => {
     dispatch({ type: "MINUS" });
   };
 
-  const toggleCounterHandler = () => {};
+  const toggleCounterHandler = () => {
+    dispatch({ type: "TOGGLE" });
+  };
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
       <div className={classes.value}>{counter} </div>
-      <div>
-        <button onClick={add}>Add</button>
-        <button onClick={increase}>Add 5 more</button>
-        <button onClick={minus}>MINUS</button>
-      </div>
+      {show && (
+        <div>
+          <button onClick={add}>Add</button>
+          <button onClick={increase}>Add 5 more</button>
+          <button onClick={minus}>MINUS</button>
+        </div>
+      )}
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
   );
--- learn-redux/src/store/index.js
+++ learn-redux/src/store/index.js
@@ -1,23 +1,35 @@
 // const redux = require("redux");
-import { createStore } from 'redux';
+import { createStore } from "redux";
 
-const counterReducer = (state = { counter: 0 }, action) => {
+const initialState = { counter: 0, showCounter: true };
+
+const counterReducer = (state = initialState, action) => {
   if (action.type === "ADD") {
     return {
       counter: (state.counter += 1),
+      showCounter: state.showCounter,
     };
   }
 
   if (action.type === "INCREASE") {
     return {
       counter: (state.counter += action.amount),
+      showCounter: state.showCounter,
     };
   }
 
   if (action.type === "MINUS") {
     return {
       counter: (state.counter -= 1),
+      showCounter: state.showCounter,
     };
+  }
+
+  if (action.type === "TOGGLE") {
+    return {
+      showCounter: !state.showCounter, 
+      counter: state.counter
+    }
   }
 
   return state;
