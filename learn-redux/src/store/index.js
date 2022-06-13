888d53c626afaec5e9327842f8def6dafb9f2a47
Toluwanimi Adeyemo <tolu.adeyemo7@gmail.com>
2022-06-13 21:07:43 +0000

    cleanup

--- learn-redux/src/store/index.js
+++ learn-redux/src/store/index.js
@@ -4,7 +4,7 @@ import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = { counter: 0, showCounter: true };
 
-createSlice({
+const counterSlice = createSlice({
   name: "counter",
   initialState,
   reducers: {
@@ -26,48 +26,6 @@ createSlice({
   },
 });
 
-const counterReducer = (state = initialState, action) => {
-  if (action.type === "ADD") {
-    return {
-      counter: (state.counter += 1),
-      showCounter: state.showCounter,
-    };
-  }
-
-  if (action.type === "INCREASE") {
-    return {
-      counter: (state.counter += action.amount),
-      showCounter: state.showCounter,
-    };
-  }
-
-  if (action.type === "MINUS") {
-    return {
-      counter: (state.counter -= 1),
-      showCounter: state.showCounter,
-    };
-  }
-
-  if (action.type === "TOGGLE") {
-    return {
-      showCounter: !state.showCounter,
-      counter: state.counter,
-    };
-  }
-
-  return state;
-};
-
-const store = createStore(counterReducer);
-
-// const counterSubscriber = () => {
-//   const latestStoreState = store.getState();
-//   console.log(latestStoreState);
-// };
-
-// store.subscribe(counterSubscriber);
-
-// store.dispatch({ type: "ADD" });
-// store.dispatch({ type: "MINUS" });
+const store = createStore(counterSlice.reducer);
 
 export default store;
