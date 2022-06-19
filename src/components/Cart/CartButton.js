36ca4019ab1d8bdc627ad61f07dda48fc52a85e4
Toluwanimi Adeyemo <tolu.adeyemo7@gmail.com>
2022-06-19 11:05:45 +0000

    worked on togglling the ui to show cart

--- src/components/Cart/CartButton.js
+++ src/components/Cart/CartButton.js
@@ -1,8 +1,16 @@
+import { useDispatch } from 'react-redux';
+import { uiActions } from '../../store/ui-slice';
 import classes from './CartButton.module.css';
 
 const CartButton = (props) => {
+  const dispatch = useDispatch();
+
+  const toggleCart = () => {
+    dispatch(uiActions.toggleCart())
+  }
+
   return (
-    <button className={classes.button}>
+    <button className={classes.button} onClick={toggleCart}>
       <span>My Cart</span>
       <span className={classes.badge}>1</span>
     </button>
