4d8df98279b54dbd94335ee7b4945c3b5623a1eb
Toluwanimi Adeyemo <tolu.adeyemo7@gmail.com>
2022-06-19 11:19:30 +0000

    add to cart fucntionality

---
+++ src/store/cart-slice.js
@@ -0,0 +1,33 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const cartSlice = createSlice({
+  name: "cart",
+  initialState: {
+    items: [],
+    totalQuantity: 0,
+    // totalAmount: 0,
+  },
+  reducers: {
+    addItemToCart: (state, action) => {
+      const newItem = action.payload;
+      const existingItem = state.items.find((item) => item.id === newItem.id);
+      if (!existingItem) {
+        state.items.push({
+          itemId: newItem.id,
+          price: newItem.price,
+          quantity: 1,
+          totalPrice: newItem.price,
+          name: newItem.name,
+        });
+      } else {
+        existingItem.quantity ++;
+        existingItem.totalPrice = existingItem.totalPrice + newItem.price;
+      }
+    },
+    removeItemFromCart: (state, action) => {},
+  },
+});
+
+export const cartActions = cartSlice.actions;
+
+export default cartSlice.reducer;
