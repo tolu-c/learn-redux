3a1dd865859adb2dd9453150225c7f458ed02cf4
Toluwanimi Adeyemo <tolu.adeyemo7@gmail.com>
2022-06-22 20:56:39 +0000

    handling http states and feedback I

--- src/App.js
+++ src/App.js
@@ -1,25 +1,80 @@
-import { useEffect } from "react";
-import { useSelector } from "react-redux";
+import { Fragment, useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
 import Cart from "./components/Cart/Cart";
 import Layout from "./components/Layout/Layout";
 import Products from "./components/Shop/Products";
+import Notification from "./components/UI/Notification";
+import { uiActions } from "./store/ui-slice";
+
+let isInitial;
 
 function App() {
+  const dispatch = useDispatch();
   const showCart = useSelector((state) => state.ui.cartIsVisible);
   const cart = useSelector((state) => state.cart);
+  const notification = useSelector((state) => state.ui.notification);
 
   useEffect(() => {
-    fetch("https://react-http-17f19-default-rtdb.firebaseio.com/cart.json", {
-      method: "PUT",
-      body: JSON.stringify(cart),
+    const sendCartData = async () => {
+      dispatch(
+        uiActions.showNotification({
+          status: "pending",
+          title: "Sending cart data.",
+          message: "Send cart data to server...",
+        })
+      );
+
+      const response = fetch(
+        "https://react-http-17f19-default-rtdb.firebaseio.com/cart.json",
+        {
+          method: "PUT",
+          body: JSON.stringify(cart),
+        }
+      );
+
+      if (!response.ok) {
+        throw new Error("Sending data failed.");
+      }
+
+      dispatch(
+        uiActions.showNotification({
+          status: "success",
+          title: "Success!.",
+          message: "Sent cart data successfully.",
+        })
+      );
+    };
+    
+    if (isInitial) {
+      isInitial = false;
+      return;
+    }
+
+    sendCartData().catch((err) => {
+      dispatch(
+        uiActions.showNotification({
+          status: "error",
+          title: "Error!.",
+          message: "Sending cart data failed.",
+        })
+      );
     });
-  }, [cart]);
+  }, [cart, dispatch]);
 
   return (
-    <Layout>
-      {showCart && <Cart />}
-      <Products />
-    </Layout>
+    <Fragment>
+      {notification && (
+        <Notification
+          status={notification.status}
+          message={notification.message}
+          title={notification.title}
+        />
+      )}
+      <Layout>
+        {showCart && <Cart />}
+        <Products />
+      </Layout>
+    </Fragment>
   );
 }
 
